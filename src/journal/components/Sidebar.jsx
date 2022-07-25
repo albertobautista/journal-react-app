b9b016672b222a10fb5ca921ce6051336d32a6d4
Alberto Bautista <alberto.bautista@capgemini.com>
2022-07-25 12:58:41 -0500

    add display name in Sidebar component

--- src/journal/components/Sidebar.jsx
+++ src/journal/components/Sidebar.jsx
@@ -13,22 +13,26 @@ import {
   ListItemText,
 } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Sidebar = ({ drawerWidth = 240 }) => {
+  const { displayName } = useSelector((state) => state.auth);
   return (
     <Box
       component="nav"
-      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
       <Drawer
         variant="permanent"
         open
         sx={{
           display: { xs: "block" },
           "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
-        }}>
+        }}
+      >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Alberto Bautista
+            {displayName}
           </Typography>
         </Toolbar>
         <Divider />
