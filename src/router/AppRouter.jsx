e353ffbe425bf21f45458c69441e374db3972b28
Alberto Bautista <alberto.bautista@capgemini.com>
2022-07-25 12:59:03 -0500

    add auth calidations in app router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { useCheckAuth } from "../hooks";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
+  const { status } = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
   }
   return (
     <Routes>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+
       {/* Login y registro */}
-      <Route path="/auth/*" element={<AuthRoutes />} />
+      {/* <Route path="/auth/*" element={<AuthRoutes />} /> */}
 
       {/* JournalApp */}
-      <Route path="/*" element={<JournalRoutes />} />
+      {/* <Route path="/*" element={<JournalRoutes />} /> */}
     </Routes>
   );
 };
